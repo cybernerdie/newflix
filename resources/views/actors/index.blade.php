3b0aaa9f00a14dd0cce212e3778ddc3982871b4f
Veecthorpaul <52431939+Veecthorpaul@users.noreply.github.com>
2020-09-30 08:05:29 +0100

    Update index.blade.php

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -10,41 +10,43 @@
 					<h2 class="content__title">Popular Actors</h2>
 				</div>
 				
-				<div class="row">
-                    <!-- card -->
-                    @foreach ($popularActors as $actor)
-					<div class="col-6 col-sm-4 col-md-3 col-xl-2">
-						<div class="card">
-							<div class="card__cover">
-                                <a href="{{ route('actors.show', $actor['id']) }}">
-                                    <img src="{{ $actor['profile_path'] }}" alt="profile image">
-                                </a>
-							</div>
-							<div class="card__content">
-                                <h3 class="card__title"><a href="{{ route('actors.show', $actor['id']) }}">{{ $actor['name'] }}</a></h3>
-								<span class="card__category" style="color: #ffd80e">
-                                    {{ $actor['known_for'] }}
-								</span>
-							</div>
-						</div>
+						<div class="container">
+                    <div class="row">
+                        <!-- card -->
+                        @foreach ($popularActors as $actor)
+                        <div class="col-6 col-sm-4 col-md-3 col-xl-2">
+                            <div class="card">
+                                <div class="card__cover">
+                                    <a href="{{ route('actors.show', $actor['id']) }}">
+                                        <img src="{{ $actor['profile_path'] }}" alt="profile image">
+                                    </a>
+                                </div>
+                                <div class="card__content">
+                                    <h3 class="card__title"><a href="{{ route('actors.show', $actor['id']) }}">{{ $actor['name'] }}</a></h3>
+                                    <span class="card__category" style="color: #ffd80e">
+                                        {{ $actor['known_for'] }}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        <!-- end card -->
                     </div>
-                    @endforeach
-					<!-- end card -->
+                      <div style=" justify-content: space-between;">
+                @if ($previous)
+                    <a href="/actors/page/{{ $previous }}" style="color: white">Previous</a>
+                @else
+                    <div></div>
+                @endif
+    
+                @if ($next)
+                    <a href="/actors/page/{{ $next }}" style="color: white">Next</a>
+                @else
+                    <div></div>
+                @endif
+    
+            </div>
                 </div>
-                  <div style=" justify-content: space-between;">
-            @if ($previous)
-                <a href="/actors/page/{{ $previous }}" style="color: white">Previous</a>
-            @else
-                <div></div>
-            @endif
-
-            @if ($next)
-                <a href="/actors/page/{{ $next }}" style="color: white">Next</a>
-            @else
-                <div></div>
-            @endif
-
-        </div>
             </div>
 		</div>
 	</section>
