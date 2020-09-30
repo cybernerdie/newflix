5ae0f9562309e4f2380af2ec63c058a2f7c4f661
Veecthorpaul <52431939+Veecthorpaul@users.noreply.github.com>
2020-09-30 08:31:28 +0100

    Update show.blade.php

--- resources/views/tv/show.blade.php
+++ resources/views/tv/show.blade.php
@@ -82,28 +82,30 @@
 				<h2 class="content__title">Casts</h2>
 			</div>
 			
-			<div class="row">
-                <!-- card -->
-                @foreach ($tvshow['cast'] as $cast)
-				<div class="col-6 col-sm-4 col-md-3 col-xl-2">
-					<div class="card">
-						<div class="card__cover">
-                            <a href="{{ route('actors.show', $cast['id']) }}">
-                                <img src="{{ $cast['profile_path'] }}" alt="actor">
-                            </a>
-						</div>
-						<div class="card__content">
-                            <h3 class="card__title"> <a href="{{ route('actors.show', $cast['id']) }}">{{ $cast['name'] }}</a></h3>
-                           
-							<span class="card__category" style="color: #ffd80e">
-								{{ $cast['character'] }}
-							</span>
-						</div>
-					</div>
+				<div class="container">
+                <div class="row">
+                    <!-- card -->
+                    @foreach ($tvshow['cast'] as $cast)
+                    <div class="col-6 col-sm-4 col-md-3 col-xl-2">
+                        <div class="card">
+                            <div class="card__cover">
+                                <a href="{{ route('actors.show', $cast['id']) }}">
+                                    <img src="{{ $cast['profile_path'] }}" alt="actor">
+                                </a>
+                            </div>
+                            <div class="card__content">
+                                <h3 class="card__title"> <a href="{{ route('actors.show', $cast['id']) }}">{{ $cast['name'] }}</a></h3>
+                               
+                                <span class="card__category" style="color: #ffd80e">
+                                    {{ $cast['character'] }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                    <!-- end card -->
                 </div>
-                @endforeach
-				<!-- end card -->
-			</div>
+            </div>
 		</div>
 	</div>
 
@@ -113,7 +115,8 @@
 				<h2 class="content__title">Images</h2>
 			</div>
 			<!-- project gallery -->
-			<div class="gallery" itemscope="">
+				<div class="container">
+            <div class="gallery" itemscope="">
 				<div class="row">
                     <!-- gallery item -->
                     @foreach ($tvshow['images'] as $image)
@@ -126,6 +129,7 @@
 					<!-- end gallery item -->
 				</div>
 			</div>
+        </div>
 			<!-- end project gallery -->
 		</div>
 	</div>
