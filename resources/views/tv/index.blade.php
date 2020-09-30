078aa2a58b7cf655c7de030782ea30549f46ad5f
Veecthorpaul <52431939+Veecthorpaul@users.noreply.github.com>
2020-09-30 08:07:05 +0100

    Update index.blade.php

--- resources/views/tv/index.blade.php
+++ resources/views/tv/index.blade.php
@@ -8,29 +8,31 @@
             <div class="col-12 mb-4">
                 <h2 class="content__title" style="color: #ffd80e">Popular Shows</h2>
             </div>
-            <div class="row">
-                <!-- card -->
-                @foreach ($popularTv as $tvshow)
-                <div class="col-6 col-sm-4 col-md-3 col-xl-2">
-                    <div class="card">
-                        <div class="card__cover">
-                                <img src="{{ $tvshow['poster_path'] }}" alt="poster">
-                                <a href="{{ route('tv.show', $tvshow['id']) }}" class="card__play">
-									<i class="icon ion-ios-play"></i>
-                                </a>
-                            <span class="card__rate card__rate--green">{{ $tvshow['vote_average'] }}</span>
-                        </div>
-                        <div class="card__content">
-                            <h3 class="card__title"><a href="{{ route('tv.show', $tvshow['id']) }}">{{ $tvshow['name'] }}</a></h3>
-                            
-                            <span class="card__category"  style="color: #ffd80e">
-                                {{ $tvshow['genres'] }}
-                            </span>
+             <div class="container">
+                <div class="row">
+                    <!-- card -->
+                    @foreach ($popularTv as $tvshow)
+                    <div class="col-6 col-sm-4 col-md-3 col-xl-2">
+                        <div class="card">
+                            <div class="card__cover">
+                                    <img src="{{ $tvshow['poster_path'] }}" alt="poster">
+                                    <a href="{{ route('tv.show', $tvshow['id']) }}" class="card__play">
+                                        <i class="icon ion-ios-play"></i>
+                                    </a>
+                                <span class="card__rate card__rate--green">{{ $tvshow['vote_average'] }}</span>
+                            </div>
+                            <div class="card__content">
+                                <h3 class="card__title"><a href="{{ route('tv.show', $tvshow['id']) }}">{{ $tvshow['name'] }}</a></h3>
+                                
+                                <span class="card__category"  style="color: #ffd80e">
+                                    {{ $tvshow['genres'] }}
+                                </span>
+                            </div>
                         </div>
                     </div>
+                    @endforeach
+                    <!-- end card -->
                 </div>
-                @endforeach
-                <!-- end card -->
             </div>
         </div>
     </div>
